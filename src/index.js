ece9735c72aabe2d1d842b7f36321e9c03380968
AvraamMavridis <avr.mav@gmail.com>
2016-08-19 15:05:40 +0200

    Detect browser that do not support the performance API

--- src/index.js
+++ src/index.js
@@ -4,13 +4,26 @@
 * @author  Avraam Mavridis      <avr.mav@gmail.com>
 *
 */
-export const perflogger = ( store ) => ( next ) => ( action ) => {
-  console.log( '%c Dispatching ', 'background: #222; color: #bada55', action );
-  const start = performance.now();
-  const result = next( action );
-  const end = performance.now();
-  console.log( `%c Action with type "${action.type}" took ${( end-start ).toFixed( 2 )} milliseconds.`, 'background: #bada55; color: #222' );
-  return result;
+
+export const perflogger = store => next => action => {
+  const browserHasPerformanceAPI = typeof window.performance === "object";
+
+  if( browserHasPerformanceAPI )
+  {
+      console.log( '%c Dispatching ', 'background: #222; color: #bada55', action );
+      const start = performance.now();
+      const result = next( action );
+      const end = performance.now();
+      console.log( `%c Action with type "${action.type}" took ${( end-start ).toFixed( 2 )} milliseconds.`, 'background: #bada55; color: #222' );
+      return result;
+  }
+  else
+  {
+      const browser = require( 'detect-browser' );
+      console.warn( browser.name + ' ' + browser.version + ' does not support the Performance API.' );
+      return next( action );
+  }
+
 };
 
 export default perflogger;
